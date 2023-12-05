5f0d6f9a20fb8c4a9cbdb05254544fcdd14faa0d
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2023-12-05 11:31:08 -0300

    feat: add part 2 solution

--- 2023/day-1/solution.mjs
+++ 2023/day-1/solution.mjs
@@ -7,38 +7,82 @@ function getInputData() {
   return data.split("\n");
 }
 
-function getDigitsFromCalibration(calibration) {
+function getDigitsFromCalibration(calibration, spelledDigits = []) {
+  // O(n) solution
   let left = 0;
   let right = calibration.length - 1;
   let digit = [null, null];
+
   while (left <= right) {
-    if (isNaN(parseInt(calibration[left]))) {
-      left++;
-    } else {
-      digit[0] = calibration[left];
+    if (digit[0] === null) {
+      if (isNaN(parseInt(calibration[left]))) {
+        for (const key in spelledDigits) {
+          const cut = calibration.slice(left, left + spelledDigits[key].length);
+          if (cut === spelledDigits[key]) {
+            digit[0] = parseInt(key) + 1;
+            break;
+          }
+        }
+        left++;
+      } else {
+        digit[0] = parseInt(calibration[left]);
+      }
     }
 
-    if (isNaN(parseInt(calibration[right]))) {
-      right--;
-    } else {
-      digit[1] = calibration[right];
+    if (digit[1] === null) {
+      if (isNaN(parseInt(calibration[right]))) {
+        for (const key in spelledDigits) {
+          const cut = calibration.slice(
+            right - spelledDigits[key].length + 1,
+            right + 1
+          );
+          if (cut === spelledDigits[key]) {
+            digit[1] = parseInt(key) + 1;
+            break;
+          }
+        }
+        right--;
+      } else {
+        digit[1] = parseInt(calibration[right]);
+      }
     }
 
     if (digit[0] !== null && digit[1] !== null) {
       break;
     }
   }
+
   return parseInt(`${digit[0]}${digit[1]}`);
 }
 
 function main() {
   const lines = getInputData();
 
-  const result = lines.reduce((acc, line) => {
+  console.time("1");
+  const result1 = lines.reduce((acc, line) => {
     return acc + getDigitsFromCalibration(line);
   }, 0);
+  console.timeEnd("1");
 
-  console.log(result);
+  const spelled = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  console.time("2");
+  const result2 = lines.reduce((acc, line) => {
+    return acc + getDigitsFromCalibration(line, spelled);
+  }, 0);
+  console.timeEnd("2");
+
+  return [result1, result2];
 }
 
 main();
