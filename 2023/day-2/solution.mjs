30ffe1d270c9a287ebf90ab4c7abe6805e92d307
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2023-12-06 12:49:21 -0300

    feat: add day 2 solution

--- 2023/day-2/solution.mjs
+++ 2023/day-2/solution.mjs
@@ -1,13 +1,79 @@
 import fs from "fs";
 
+function toSum(acc, val) {
+  return acc + val;
+}
+
 function getInputData() {
-  const data = fs.readFileSync("input", "utf-8");
-  return data.split("\n");
+  const lines = fs.readFileSync("input", "utf-8").split("\n");
+  const games = new Map();
+  for (const game of lines) {
+    const [id, plays] = game.split(":");
+    const sets = plays.split(";");
+    games.set(+id.replace("Game ", ""), sets.reduce(toSetsOfCubes, []));
+  }
+  return games;
+}
+
+function toSetsOfCubes(acc, set) {
+  return [
+    ...acc,
+    {
+      ...Object.fromEntries(
+        set.split(",").map((cubes) => cubes.split(" ").reverse())
+      ),
+    },
+  ];
+}
+
+function computePossibleGamesWithCubes(games, red = 0, green = 0, blue = 0) {
+  const possibles = new Map();
+  const compare = {
+    red: (num) => parseInt(num) <= red,
+    green: (num) => parseInt(num) <= green,
+    blue: (num) => parseInt(num) <= blue,
+  };
+
+  for (const [id, game] of games) {
+    const isPlayPossible = [];
+    for (const play of game) {
+      const cubes = Object.entries(play);
+      isPlayPossible.push(cubes.every(([color, num]) => compare[color](num)));
+    }
+    if (!isPlayPossible.includes(false)) {
+      possibles.set(id, game);
+    }
+  }
+  return possibles;
+}
+
+function computeLeastAmountOfCubes(games) {
+  const powers = [];
+
+  for (const [_id, game] of games) {
+    const least = {};
+
+    for (const play of game) {
+      for (const cube in play) {
+        least[cube] = Math.max(least[cube] || 0, parseInt(play[cube]));
+      }
+    }
+    powers.push(Object.values(least).reduce((acc, num) => acc * num, 1));
+  }
+
+  return powers.reduce(toSum, 0);
 }
 
 function main() {
-  const lines = getInputData();
-  console.log(lines);
+  const games = getInputData();
+  console.time("Part 1");
+  const possibles = computePossibleGamesWithCubes(games, 12, 13, 14);
+  console.timeEnd("Part 1");
+  console.time("Part 2");
+  const least = computeLeastAmountOfCubes(games, 12, 13, 14);
+  console.timeEnd("Part 2");
+
+  return [[...possibles.keys()].reduce(toSum, 0), least];
 }
 
-main();
+console.log(main());
